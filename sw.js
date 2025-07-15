c6ce2bec5b43a23c2bb25c6b212ab80dc616c7d7
Ryusei801045 <ryuse8010@gmail.com>
2025-07-15 22:39:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,59 @@
 const CACHE_NAME = 'todo-pwa-cache-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './styles.css',
-    './app.js',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
+// インストールイベント: キャッシュにファイルを保存
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(key => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }))
-        )
-    );
-});
-
+// フェッチイベント: キャッシュからリソースを返す
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                // キャッシュにレスポンスがあればそれを返す
+                if (response) {
+                    return response;
+                }
+                // なければネットワークから取得
+                return fetch(event.request);
+            })
+    );
+});
+
+// アクティベートイベント: 古いキャッシュを削除
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        // 現在のキャッシュ名に含まれていない古いキャッシュを削除
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
